5d8fab3baa8c7e70355bfa136b8514704ceb0464
Chitranshi Tayal <161892527+tayalchitranshi@users.noreply.github.com>
2024-07-02 21:00:02 +0530

    Add files via upload

---
+++ Coffee Shop Sales SQL.sql
@@ -0,0 +1,189 @@
+USE coffee_shop_sales_db;
+
+SELECT * FROM coffee_shop_sales;
+DESCRIBE coffee_shop_sales;
+
+SELECT * FROM coffee_shop_sales;
+
+#CONVERT DATE (transaction_date) COLUMN TO PROPER DATE FORMAT
+UPDATE coffee_shop_sales
+SET transaction_date = STR_TO_DATE(transaction_date, '%d/%m/%Y');
+
+#ALTER DATE (transaction_date) COLUMN TO DATE DATA TYPE
+ALTER TABLE coffee_shop_sales
+MODIFY COLUMN transaction_date DATE;
+
+DESCRIBE coffee_shop_sales;
+
+#CONVERT TIME (transaction_time)  COLUMN TO PROPER DATE FORMAT
+UPDATE coffee_shop_sales
+SET transaction_time =str_to_date(transaction_time,'%H:%i:%s');
+
+#ALTER TIME (transaction_time) COLUMN TO DATE DATA TYPE
+ALTER TABLE coffee_shop_sales
+MODIFY COLUMN transaction_time TIME;
+
+DESCRIBE coffee_shop_sales;
+
+SELECT * FROM coffee_shop_sales;
+
+#TOTAL SALES 
+SELECT ROUND(SUM(unit_price * transaction_qty)) as Total_Sales 
+FROM coffee_shop_sales 
+WHERE MONTH(transaction_date) = 5 -- for month of (CM-May)
+
+SELECT * FROM coffee_shop_sales;
+
+#TOTAL SALES KPI - Month ON MONTH DIFFERENCE AND MONTH ON MONTH GROWTH
+SELECT 
+    MONTH(transaction_date) AS month,
+    ROUND(SUM(unit_price * transaction_qty)) AS total_sales,
+    (SUM(unit_price * transaction_qty) - LAG(SUM(unit_price * transaction_qty), 1)
+    OVER (ORDER BY MONTH(transaction_date))) / LAG(SUM(unit_price * transaction_qty), 1) 
+    OVER (ORDER BY MONTH(transaction_date)) * 100 AS mom_increase_percentage
+FROM 
+    coffee_shop_sales
+WHERE 
+    MONTH(transaction_date) IN (4, 5) -- for months of April and May
+GROUP BY 
+    MONTH(transaction_date)
+ORDER BY 
+    MONTH(transaction_date);
+    
+SELECT * FROM coffee_shop_sales;
+
+#TOTAL ORDERS
+SELECT COUNT(transaction_id) as Total_Orders
+FROM coffee_shop_sales 
+WHERE MONTH (transaction_date)= 5
+
+#TOTAL ORDERS KPI - MOM DIFFERENCE AND MOM GROWTH
+SELECT 
+    MONTH(transaction_date) AS month,
+    ROUND(COUNT(transaction_id)) AS total_orders,
+    (COUNT(transaction_id) - LAG(COUNT(transaction_id), 1) 
+    OVER (ORDER BY MONTH(transaction_date))) / LAG(COUNT(transaction_id), 1)
+    OVER (ORDER BY MONTH(transaction_date)) * 100 AS mom_increase_percentage
+FROM 
+    coffee_shop_sales
+WHERE 
+    MONTH(transaction_date) IN (4, 5) -- for April and May
+GROUP BY 
+    MONTH(transaction_date)
+ORDER BY 
+    MONTH(transaction_date);
+
+#TOTAL QUANTITY SOLD
+SELECT SUM(transaction_qty) as Total_Quantity_Sold
+FROM coffee_shop_sales 
+WHERE MONTH(transaction_date) = 5
+
+#TOTAL QUANTITY SOLD KPI - MOM DIFFERENCE AND MOM GROWTH
+SELECT 
+    MONTH(transaction_date) AS month,
+    ROUND(SUM(transaction_qty)) AS total_quantity_sold,
+    (SUM(transaction_qty) - LAG(SUM(transaction_qty), 1) 
+    OVER (ORDER BY MONTH(transaction_date))) / LAG(SUM(transaction_qty), 1) 
+    OVER (ORDER BY MONTH(transaction_date)) * 100 AS mom_increase_percentage
+FROM 
+    coffee_shop_sales
+WHERE 
+    MONTH(transaction_date) IN (4, 5)   -- for April and May
+GROUP BY 
+    MONTH(transaction_date)
+ORDER BY 
+    MONTH(transaction_date);
+
+#CALENDAR TABLE – DAILY SALES, QUANTITY and TOTAL ORDERS
+SELECT
+    SUM(unit_price * transaction_qty) AS total_sales,
+    SUM(transaction_qty) AS total_quantity_sold,
+    COUNT(transaction_id) AS total_orders
+FROM 
+    coffee_shop_sales
+WHERE 
+    transaction_date = '2023-05-18';
+
+#SALES TREND OVER PERIOD
+SELECT AVG(total_sales) AS average_sales
+FROM (
+    SELECT 
+        SUM(unit_price * transaction_qty) AS total_sales
+    FROM 
+        coffee_shop_sales
+	WHERE 
+        MONTH(transaction_date) = 5  -- Filter for May
+    GROUP BY 
+        transaction_date
+) AS internal_query;
+
+#DAILY SALES FOR MONTH SELECTED
+SELECT 
+    DAY(transaction_date) AS day_of_month,
+    ROUND(SUM(unit_price * transaction_qty),1) AS total_sales
+FROM 
+    coffee_shop_sales
+WHERE 
+    MONTH(transaction_date) = 5
+GROUP BY 
+    DAY(transaction_date)
+ORDER BY 
+    DAY(transaction_date);
+    
+#SALES BY STORE LOCATION
+SELECT 
+	store_location,
+	SUM(unit_price * transaction_qty) as Total_Sales
+FROM coffee_shop_sales
+WHERE
+	MONTH(transaction_date) =5 
+GROUP BY store_location
+ORDER BY 	SUM(unit_price * transaction_qty) DESC
+
+#SALES BY PRODUCT CATEGORY
+SELECT 
+	product_category,
+	ROUND(SUM(unit_price * transaction_qty),1) as Total_Sales
+FROM coffee_shop_sales
+WHERE
+	MONTH(transaction_date) = 5 
+GROUP BY product_category
+ORDER BY SUM(unit_price * transaction_qty) DESC
+
+#SALES BY PRODUCTS (TOP 10)
+SELECT 
+	product_type,
+	ROUND(SUM(unit_price * transaction_qty),1) as Total_Sales
+FROM coffee_shop_sales
+WHERE
+	MONTH(transaction_date) = 5 
+GROUP BY product_type
+ORDER BY SUM(unit_price * transaction_qty) DESC
+LIMIT 10
+
+#SALES BY DAY | HOUR
+SELECT 
+    ROUND(SUM(unit_price * transaction_qty)) AS Total_Sales,
+    SUM(transaction_qty) AS Total_Quantity,
+    COUNT(*) AS Total_Orders
+FROM 
+    coffee_shop_sales
+WHERE 
+    DAYOFWEEK(transaction_date) = 3
+    AND HOUR(transaction_time) = 8
+    AND MONTH(transaction_date) = 5;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
